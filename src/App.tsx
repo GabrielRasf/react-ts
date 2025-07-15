c622714af0ac08263f025c2ff0cda556feca3f38
Gabriel R <gabrielrasf@hotmail.com>
2025-07-14 22:38:13 -0300

    update

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+// PascalCase
+// App
+// ExemploDeNomeDeComponente
+
+export function App () {
+  console.log('Oi')
+
+  return (
+    // react fragment <> e </>
+    <> 
+      <h1>Olá mundo (do App)</h1>
+      <h1>Olá mundo (do main)</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro doloremque et deserunt quae laboriosam in, magnam rem quidem quam culpa deleniti sint amet esse totam laudantium, non laborum ipsam autem.</p>
+    </>
+  )
+}
+
